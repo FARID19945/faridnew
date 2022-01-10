d2eacea1ed081231b1121e5f11b891d094150097
Farid <farid1994@inbox.rul>
2022-01-11 02:52:32 +0300

    first blood

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// let answer = [];
+// answer[0]= prompt("kak ti dada?","");
+// answer[1]= prompt("kak ti da?","");
+
+// document.write(answer);
+// const category= 'toys'
+// console.log(`'http://someurl.com /444${category}`);
+const user="ivan";
+    alert( `Privet, ${user}`  )
+console.log(5+ +"dd");
+let a =10,
+    d=9;
+// a++;
+// d--;
+console.log(++a);
+console.log(--d);
+
+const isChecked=true 
+const isFalse=true
+const dada=false
+console.log(isChecked&&isFalse&&dada);
